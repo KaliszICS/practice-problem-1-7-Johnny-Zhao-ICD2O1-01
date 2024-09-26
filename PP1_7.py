82a4e12b737d7a33d261c5138773ecb11d92482c
Johnny-Zhao-ICD2O1-01 <johnny.zhao@student.tdsb.on.ca>
2024-09-26 13:20:15 +0000

    commit

--- PP1_7.py
+++ PP1_7.py
@@ -1,25 +1,38 @@
+def q1():
+  trueOrFalseQ1 = True
+  print(trueOrFalseQ1)
 
+def q2():
+  numQ2 = input("Input an integer: ")
+  numQ2 = int(numQ2)
+  bool2 = 5 < numQ2
+  print(bool2)
 
-q1():
-  #Write code here
+def q3():
+  letterQ3 = input("Input the letter a: ")
+  letterQ3 = str(letterQ3)
+  bool3 = "a" == letterQ3
+  print(bool3)
 
-q2():
-  #Write code here
+def q4():
+  wordQ4 = input("Input a word earlier in the dictionary than google: ")
+  wordQ4 = str(wordQ4)
+  bool4 = wordQ4 < "google"
+  print(bool4)
 
-q3():
-  #Write code here
-
-q4():
-  #Write code here
-
-q5():
-  #Write code here
+def q5():
+  numQ51 = input("Input an integer: ")
+  numQ52 = input("Input another integer: ")
+  numQ51 = int(numQ51)
+  numQ52 = int(numQ52)
+  bool5 = numQ51 * numQ52 > 40
+  print(f"Your numbers multiplied together are greater than 40: {bool5}")
 
 #Do edit the code below
 #Comment the lines below when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+#q1()
+#q2()
+#q3()
+#q4()
+#q5()
